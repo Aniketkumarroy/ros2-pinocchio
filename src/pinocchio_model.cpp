bb1ee751e8bf87260f617e01c3ed78d21cf43bd6
Aniket Kumar Roy <aniketroy769@gmail.com>
2025-04-05 14:16:01 +0530

    performing forward kinematics and gettingtransform

--- src/pinocchio_model.cpp
+++ src/pinocchio_model.cpp
@@ -1,18 +1,21 @@
 #include <filesystem>
 
-#include "ament_index_cpp/get_package_share_directory.hpp"
-#include "pinocchio/algorithm/frames.hpp"
-#include "pinocchio/algorithm/geometry.hpp"
-#include "<pinocchio/algorithm/jacobian.hpp>"
-#include "<pinocchio/algorithm/joint-configuration.hpp>"
+#include <ament_index_cpp/get_package_share_directory.hpp>
+#include <pinocchio/algorithm/frames.hpp>
+#include <pinocchio/algorithm/geometry.hpp>
+#include <pinocchio/algorithm/jacobian.hpp>
+#include <pinocchio/algorithm/joint-configuration.hpp>
 // #include "pinocchio/collision/broadphase.hpp"
-#include <map>
 #include <pinocchio/parsers/srdf.hpp>
 #include <pinocchio/parsers/urdf.hpp>
+#include <unordered_map>
 
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 #include <string>
+
+using Scalar = double; // if we switch it to other type then we might need to
+                       // adjust datatype of pinocchio accordingly
 
 class RobotDescripionSubscriber : public rclcpp::Node {
 public:
@@ -27,7 +30,7 @@ private:
   void loadPinocchioModelFromXML(const std_msgs::msg::String &msg) {
     pinocchio::urdf::buildModelFromXML(msg.data, model_);
     RCLCPP_INFO(this->get_logger(),
-                "RobotDescripionSubscriber::loadPinocchioModelFromXML "
+                "[RobotDescripionSubscriber::loadPinocchioModelFromXML] "
                 "Pinocchio model loaded with %d joints.",
                 model_.njoints);
 
@@ -49,17 +52,18 @@ private:
 
     collision_model_.addAllCollisionPairs();
     RCLCPP_INFO(this->get_logger(),
-                "RobotDescripionSubscriber::loadPinocchioModelFromXML "
+                "[RobotDescripionSubscriber::loadPinocchioModelFromXML] "
                 "Pinocchio collision model loaded sucessfully");
+    is_loaded_ = true;
 
     initializeModelData();
     findJointIds();
   }
 
   void findJointIds() {
-    if (model_.njoints == 0) {
+    if (is_loaded_ == false || model_.njoints == 0) {
       RCLCPP_INFO(this->get_logger(),
-                  "RobotDescripionSubscriber::findJointIds model is not "
+                  "[RobotDescripionSubscriber::findJointIds] model is not "
                   "loaded, no of joints is %d",
                   model_.njoints);
       return;
@@ -73,19 +77,65 @@ private:
   }
 
   void initializeModelData() {
+    if (is_loaded_ == false || model_.njoints == 0) {
+      RCLCPP_INFO(this->get_logger(),
+                  "[RobotDescripionSubscriber::initializeModelData] model is not "
+                  "loaded, no of joints is %d",
+                  model_.njoints);
+      return;
+    }
+
     model_data_ = pinocchio::Data(model_);
     visual_data_ = pinocchio::GeometryData(visual_model_);
     collision_data_ = pinocchio::GeometryData(collision_model_);
 
     RCLCPP_INFO(this->get_logger(),
-                "RobotDescripionSubscriber::initializeModelData "
+                "[RobotDescripionSubscriber::initializeModelData] "
                 "Pinocchio model data initialized sucessfully");
+  }
+
+  void
+  forwardKinematics(const Eigen::Matrix<Scalar, Eigen::Dynamic, 1> &q_joints) {
+
+    if (is_loaded_ == false || model_.njoints == 0) {
+      RCLCPP_INFO(this->get_logger(),
+                  "[RobotDescripionSubscriber::forwardKinematics] model is not "
+                  "loaded, no of joints is %d",
+                  model_.njoints);
+      return;
+    }
+
+    pinocchio::forwardKinematics(model_, model_data_, q_joints);
+    pinocchio::updateFramePlacements(model_, model_data_);
+    pinocchio::updateGeometryPlacements(model_, model_data_, collision_model_,
+                                        collision_data_);
+    pinocchio::updateGeometryPlacements(model_, model_data_, visual_model_,
+                                        visual_data_);
+
+    updateTransform();
+  }
+
+  void updateTransform() {
+
+    if (is_loaded_ == false || model_.njoints == 0) {
+      RCLCPP_INFO(this->get_logger(),
+                  "[RobotDescripionSubscriber::updateTransform] model is not "
+                  "loaded, no of joints is %d",
+                  model_.njoints);
+      return;
+    }
+
+    pinocchio::JointIndex _n_joints = model_.njoints;
+    for (pinocchio::JointIndex i = 1; i < _n_joints; ++i) {
+      pinocchio::SE3 _joint_transform = model_data_.oMi[i];
+      joints_transform_[i].linear() = _joint_transform.rotation();
+      joints_transform_[i].translation() = _joint_transform.translation();
+    }
   }
 
   void robotDescSubs(const std_msgs::msg::String &msg) {
     if (!is_loaded_) {
       loadPinocchioModelFromXML(msg);
-      is_loaded_ = true;
     }
   }
 
@@ -99,7 +149,10 @@ private:
   pinocchio::GeometryData visual_data_;
   pinocchio::GeometryData collision_data_;
 
-  std::map<std::string, pinocchio::JointIndex> joint_id_map_;
+  std::unordered_map<std::string, pinocchio::JointIndex> joint_id_map_;
+  std::unordered_map<pinocchio::JointIndex,
+                     Eigen::Transform<Scalar, 3, Eigen::Isometry>>
+      joints_transform_;
 };
 
 int main(int argc, char *argv[]) {
